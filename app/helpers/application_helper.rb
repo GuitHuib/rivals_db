b71a55ccd8a4212f4c6cd0471dd6a1aa359a39e4
= <=>
2021-11-02 14:41:12 -0500

    add title and favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base = "RivalsDb"
+    if page_title.empty?
+      base
+    else
+      page_title + " | " + base
+    end
+  end
 end
